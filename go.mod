acec5bbc50c1865af23ae8ec76302672a59089ae
Alexandr Golovachev <gpunker@gmail.com>
2026-08-21 19:24:01 +0700

    tried to answer by bot command by long using long polling

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/gpunker/rss-telegram-bot
 
 go 1.26.6
+
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/joho/godotenv v1.5.1
+)
--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/joho/godotenv"
+)
+
 func main() {
-	
+	loadEnvironment()
+
+	botID := os.Getenv("BOT_ID")
+
+	bot, err := tgbotapi.NewBotAPI(botID)
+
+	if err != nil {
+		panic(err)
+	}
+
+    isDebug, err := strconv.ParseBool(os.Getenv("DEBUG"))
+    if err != nil {
+        panic(err)
+    }
+	bot.Debug = isDebug
+
+	updateConfig := tgbotapi.NewUpdate(0)
+	updateConfig.Timeout = 30
+
+	updates := bot.GetUpdatesChan(updateConfig)
+
+	for update := range updates {
+		if update.Message == nil || update.Message.Text != "/hello" {
+			continue
+		}
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "world")
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		if _, err := bot.Send(msg); err != nil {
+			panic(err)
+		}
+	}
+}
+
+func loadEnvironment() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file. '.env' file not found.")
+	}
 }
